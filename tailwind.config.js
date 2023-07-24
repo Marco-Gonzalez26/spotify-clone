f219cf3aaac5fe2ec9d89a2f64042a7531f3e2c8
Marco Gonzalez <magp2606@gmail.com>
2023-07-23 23:27:50 -0500

    Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,21 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    colors: {
+      'base': '#121212',
+      'base-highlight': '#1a1a1a',
+      'spotify-green': '#1ed760'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
